2a917a5a42832144f97bc786afa7537e442b4e89
cy <310486578@qq.com>
2020-12-23 14:33:49 +0800

    commit

--- tests/carInOrder/test_CarInOrder.py
+++ tests/carInOrder/test_CarInOrder.py
@@ -12,7 +12,9 @@ vin = None
 expressNo = None
 carInOrderId = None
 carInOrderDetails = None
+contractId = None
 get_vin = Publice()
+
 
 class Test_CarInOrder(object):
 
@@ -36,7 +38,7 @@ class Test_CarInOrder(object):
         carInOrderDetails = session.query(car_in_order_details).filter(
             car_in_order_details.vin == vin).first().id
 
-    # @pytest.mark.skip(reason='test')
+    @pytest.mark.skip(reason='test')
     @pytest.mark.run(order=2)
     @pytest.mark.datafile("110_data/carInOrder/CarInOrderAudit.yml")
     def test_CarInOrderAudit(self, env, parameters, token):
@@ -44,7 +46,7 @@ class Test_CarInOrder(object):
         r = parameters_request(env, parameters, token)
         res_validate(r.json(), parameters["validate"], r.status_code)
 
-    # @pytest.mark.skip(reason='test')
+    @pytest.mark.skip(reason='test')
     @pytest.mark.run(order=3)
     @pytest.mark.datafile("110_data/carInOrder/CarInStock.yml")
     def test_CarInStock(self, env, parameters, token):
@@ -53,7 +55,7 @@ class Test_CarInOrder(object):
         r = parameters_request(env, parameters, token)
         res_validate(r.json(), parameters["validate"], r.status_code)
 
-    # @pytest.mark.skip(reason='test')
+    @pytest.mark.skip(reason='test')
     @pytest.mark.run(order=4)
     @pytest.mark.datafile("110_data/carInOrder/CarInOrderFinalAudit.yml")
     def test_CarInOrderFinalAudit(self, env, parameters, token):
@@ -71,3 +73,32 @@ class Test_CarInOrder(object):
                "DELETE FROM car_stock WHERE vin = '%s'" % vin)
         db = MySql()
         db.mysql_update(sql)
+
+    @pytest.mark.skip(reason='test')
+    @pytest.mark.run(order=6)
+    @pytest.mark.datafile("110_data/carInOrder/CarSalesContractSave.yml")
+    def test_CarSalesContractSave(self, env, parameters, token):
+        r = parameters_request(env, parameters, token)
+        res_validate(r.json(), parameters["validate"], r.status_code)
+        global contractId
+        contractId = r.json()['data']['id']
+
+    @pytest.mark.skip(reason='test')
+    @pytest.mark.run(order=7)
+    @pytest.mark.datafile("110_data/carInOrder/CarSalesContractAudit.yml")
+    def test_CarSalesContractAudit(self, env, parameters, token):
+        parameters['request']['data']['contractId'] = contractId
+        r = parameters_request(env, parameters, token)
+        res_validate(r.json(), parameters["validate"], r.status_code)
+
+    @pytest.mark.skip(reason='test')
+    @pytest.mark.run(order=8)
+    @pytest.mark.datafile("110_data/carInOrder/contractAssgnationCarOrReleaseCar.yml")
+    def test_contractAssgnationCarOrReleaseCar(self, env, parameters, token):
+        sql = ("SELECT id FROM car_info WHERE vin = '%s'" % vin)
+        db = MySql()
+        carInfoId = db.mysql_select(sql)
+        parameters['request']['data']['contractId'] = contractId
+        parameters['request']['data']['carInfoId'] = carInfoId
+        r = parameters_request(env, parameters, token)
+        res_validate(r.json(), parameters["validate"], r.status_code)
