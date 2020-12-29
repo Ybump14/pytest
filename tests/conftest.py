6fe00f8d1994d441fdacdddc3b55f53d1758b78c
cy <310486578@qq.com>
2020-12-29 15:43:34 +0800

    commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
+import inspect
 import json
 import os, sys, requests
 
+import allure
 import yaml
 import pytest
+from _pytest import logging
+from _pytest.assertion.util import assertrepr_compare
 
 sys.path.append(os.getcwd())
 from utils.commlib import decode
@@ -88,13 +92,13 @@ def token_financial(env):
         token = res["data"]["token"]
         return token
 
-#
-# def pytest_assertrepr_compare(config, op, left, right):
-#     left_name, right_name = inspect.stack()[7].code_context[0].lstrip().lstrip('assert').rstrip('\n').split(op)
-#     pytest_output = assertrepr_compare(config, op, left, right)
-#     logging.debug("{0} is\n {1}".format(left_name, left))
-#     logging.debug("{0} is\n {1}".format(right_name, right))
-#     with allure.step("校验结果"):
-#         allure.attach(str(left), left_name)
-#         allure.attach(str(right), right_name)
-#     return pytest_output
+
+def pytest_assertrepr_compare(config, op, left, right):
+    left_name, right_name = inspect.stack()[7].code_context[0].lstrip().lstrip('assert').rstrip('\n').split(op)
+    pytest_output = assertrepr_compare(config, op, left, right)
+    logging.debug("{0} is\n {1}".format(left_name, left))
+    logging.debug("{0} is\n {1}".format(right_name, right))
+    with allure.step("校验结果"):
+        allure.attach(str(left), left_name)
+        allure.attach(str(right), right_name)
+    return pytest_output
