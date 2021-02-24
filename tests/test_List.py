4b30427282aabe6906f53728f26476d4c32a89e3
cy <310486578@qq.com>
2021-02-24 15:10:02 +0800

    commit

--- tests/test_List.py
+++ tests/test_List.py
@@ -5,103 +5,103 @@ from tests.conftest import parameters_request
 
 class Test_List():
 
-    @pytest.mark.datafile("data/carSalesDetailReport.yml")
+    @pytest.mark.datafile("data/oa/carSalesDetailReport.yml")
     def test_carSalesDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/repairOrderDetailReport.yml")
+    @pytest.mark.datafile("data/oa/repairOrderDetailReport.yml")
     def test_repairOrderDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/activityRepairPackageOrder.yml")
+    @pytest.mark.datafile("data/oa/activityRepairPackageOrder.yml")
     def test_activityRepairPackageOrder(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/externalAccessoriesOrder.yml")
+    @pytest.mark.datafile("data/oa/externalAccessoriesOrder.yml")
     def test_externalAccessoriesOrder(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/externalBoutiqueOrder.yml")
+    @pytest.mark.datafile("data/oa/externalBoutiqueOrder.yml")
     def test_externalBoutiqueOrder(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/accessoriesReceiveOrderDetailReport.yml")
+    @pytest.mark.datafile("data/oa/accessoriesReceiveOrderDetailReport.yml")
     def test_accessoriesReceiveOrderDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/boutiqueReceiveOrderDetailReport.yml")
+    @pytest.mark.datafile("data/oa/boutiqueReceiveOrderDetailReport.yml")
     def test_boutiqueReceiveOrderDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/insuranceOrderDetailReport.yml")
+    @pytest.mark.datafile("data/oa/insuranceOrderDetailReport.yml")
     def test_insuranceOrderDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/otherReceiveOrderDetailReport.yml")
+    @pytest.mark.datafile("data/oa/otherReceiveOrderDetailReport.yml")
     def test_otherReceiveOrderDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/carRedemOrderFinancingReport.yml")
+    @pytest.mark.datafile("data/oa/carRedemOrderFinancingReport.yml")
     def test_otherReceiveOrderDetailReport(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/teamManagement.yml")
+    @pytest.mark.datafile("data/oa/teamManagement.yml")
     def test_teamManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/customerManagement.yml")
+    @pytest.mark.datafile("data/oa/customerManagement.yml")
     def test_customerManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/customerServiceManagement.yml")
+    @pytest.mark.datafile("data/oa/customerServiceManagement.yml")
     def test_customerServiceManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/partsManagement.yml")
+    @pytest.mark.datafile("data/oa/partsManagement.yml")
     def test_partsManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/boutiqueManagement.yml")
+    @pytest.mark.datafile("data/oa/boutiqueManagement.yml")
     def test_boutiqueManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/vehicleManagement.yml")
+    @pytest.mark.datafile("data/oa/vehicleManagement.yml")
     def test_vehicleManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/vehicleSaleManagement.yml")
+    @pytest.mark.datafile("data/oa/vehicleSaleManagement.yml")
     def test_vehicleSaleManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/repairReceptionManagement.yml")
+    @pytest.mark.datafile("data/oa/repairReceptionManagement.yml")
     def test_repairReceptionManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/insuranceManagement.yml")
+    @pytest.mark.datafile("data/oa/insuranceManagement.yml")
     def test_insuranceManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/marketingManagement.yml")
+    @pytest.mark.datafile("data/oa/marketingManagement.yml")
     def test_marketingManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/external_take_Management.yml")
+    @pytest.mark.datafile("data/oa/external_take_Management.yml")
     def test_external_take_Management(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/receiptManagement.yml")
+    @pytest.mark.datafile("data/oa/receiptManagement.yml")
     def test_receiptManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/ticketManagement.yml")
+    @pytest.mark.datafile("data/oa/ticketManagement.yml")
     def test_ticketManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/financeManagement.yml")
+    @pytest.mark.datafile("data/oa/financeManagement.yml")
     def test_financeManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
-    @pytest.mark.datafile("data/parameter_auth_systemManagement.yml")
+    @pytest.mark.datafile("data/oa/parameter_auth_systemManagement.yml")
     def test_parameter_auth_systemManagement(self, env, parameters, token_oa):
         parameters_request(env, parameters, token_oa, Environmental='oa')
 
